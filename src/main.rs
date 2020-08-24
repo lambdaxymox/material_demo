22964fc3e821a6156781702804fbe8e49f33dbc8
LambdaXymox <lambda.xymox@gmail.com>
2020-08-24 00:18:40 -0500

    Include more boilerplate for sending data to the GPU.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,68 @@ mod gl {
 mod backend;
 mod mesh;
 
+use glfw::{Action, Context, Key};
+use gl::types::{GLfloat, GLint, GLuint, GLvoid, GLsizeiptr};
+use log::{info};
+use tex_atlas::TextureAtlas2D;
+use std::io;
+
+
+// OpenGL extension constants.
+const GL_TEXTURE_MAX_ANISOTROPY_EXT: u32 = 0x84FE;
+const GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT: u32 = 0x84FF;
+
+
+/// Load texture image into the GPU.
+fn send_to_gpu_texture(atlas: &TextureAtlas2D, wrapping_mode: GLuint) -> Result<GLuint, String> {
+    let mut tex = 0;
+    unsafe {
+        gl::GenTextures(1, &mut tex);
+        gl::ActiveTexture(gl::TEXTURE0);
+        gl::BindTexture(gl::TEXTURE_2D, tex);
+        gl::TexImage2D(
+            gl::TEXTURE_2D, 0, gl::RGBA as i32, atlas.width as i32, atlas.height as i32, 0,
+            gl::RGBA, gl::UNSIGNED_BYTE,
+            atlas.as_ptr() as *const GLvoid
+        );
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, wrapping_mode as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, wrapping_mode as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR as GLint);
+    }
+    debug_assert!(tex > 0);
+
+    let mut max_aniso = 0.0;
+    unsafe {
+        gl::GetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &mut max_aniso);
+        // Set the maximum!
+        gl::TexParameterf(gl::TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max_aniso);
+    }
+
+    Ok(tex)
+}
+
+#[derive(Copy, Clone)]
+struct ShaderSource {
+    vert_name: &'static str,
+    vert_source: &'static str,
+    frag_name: &'static str,
+    frag_source: &'static str,
+}
+
+fn send_to_gpu_shaders(game: &mut backend::GLState, source: ShaderSource) -> GLuint {
+    let mut vert_reader = io::Cursor::new(source.vert_source);
+    let mut frag_reader = io::Cursor::new(source.frag_source);
+    let sp = backend::create_program_from_reader(
+        game,
+        &mut vert_reader, source.vert_name,
+        &mut frag_reader, source.frag_name
+    ).unwrap();
+    debug_assert!(sp > 0);
+
+    sp
+}
 
 fn main() {
     println!("Hello, world!");
