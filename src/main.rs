4358361d7f6ade44b66d3d1cc14bfa628c935a04
LambdaXymox <lambda.xymox@gmail.com>
2020-08-31 01:17:51 -0500

    Remove dead code.

--- src/main.rs
+++ src/main.rs
@@ -42,10 +42,6 @@ use std::mem;
 use std::ptr;
 
 
-// OpenGL extension constants.
-const GL_TEXTURE_MAX_ANISOTROPY_EXT: u32 = 0x84FE;
-const GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT: u32 = 0x84FF;
-
 // Default value for the color buffer.
 const CLEAR_COLOR: [f32; 4] = [0.2_f32, 0.2_f32, 0.2_f32, 1.0_f32];
 // Default value for the depth buffer.
@@ -193,37 +189,6 @@ fn send_to_gpu_uniforms_material(shader: GLuint, material: &Material) {
     }
 }
 
-/*
-/// Load texture image into the GPU.
-fn send_to_gpu_texture(atlas: &TextureAtlas2D, wrapping_mode: GLuint) -> Result<GLuint, String> {
-    let mut tex = 0;
-    unsafe {
-        gl::GenTextures(1, &mut tex);
-        gl::ActiveTexture(gl::TEXTURE0);
-        gl::BindTexture(gl::TEXTURE_2D, tex);
-        gl::TexImage2D(
-            gl::TEXTURE_2D, 0, gl::RGBA as i32, atlas.width as i32, atlas.height as i32, 0,
-            gl::RGBA, gl::UNSIGNED_BYTE,
-            atlas.as_ptr() as *const GLvoid
-        );
-        gl::GenerateMipmap(gl::TEXTURE_2D);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, wrapping_mode as GLint);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, wrapping_mode as GLint);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR as GLint);
-    }
-    debug_assert!(tex > 0);
-
-    let mut max_aniso = 0.0;
-    unsafe {
-        gl::GetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &mut max_aniso);
-        // Set the maximum!
-        gl::TexParameterf(gl::TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max_aniso);
-    }
-
-    Ok(tex)
-}
-*/
 fn send_to_gpu_mesh(shader: GLuint, mesh: &ObjMesh) -> (GLuint, GLuint, GLuint, GLuint) {
     let v_pos_loc = unsafe {
         gl::GetAttribLocation(shader, backend::gl_str("v_pos").as_ptr())
