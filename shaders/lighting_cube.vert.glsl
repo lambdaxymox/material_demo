5cd4a192492e1b75c2f9ed5f6ef03d53525d6b00
LambdaXymox <lambda.xymox@gmail.com>
2020-09-01 13:36:49 -0500

    Include shader for displaying cube light.

---
+++ shaders/lighting_cube.vert.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+struct Camera {
+    mat4 proj_mat;
+    mat4 view_mat;
+}
+
+layout (location = 0) in vec3 v_pos;
+
+uniform mat4 model_mat;
+uniform Camera camera;
+
+
+void main() {
+    gl_Position = camera.proj_mat * camera.view_mat * model_mat * vec4(v_pos, 1.0);
+}
